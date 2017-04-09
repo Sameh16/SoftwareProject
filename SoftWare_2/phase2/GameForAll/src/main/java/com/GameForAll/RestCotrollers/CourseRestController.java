263a903489d1f9a82b159665c3d183bfa223301b
joker mohamed <joker mohamed>
2017-04-09 23:18:59 +0200

    after edit

--- SoftWare_2/phase2/GameForAll/src/main/java/com/GameForAll/RestCotrollers/CourseRestController.java
+++ SoftWare_2/phase2/GameForAll/src/main/java/com/GameForAll/RestCotrollers/CourseRestController.java
@@ -1,5 +1,6 @@
 package com.GameForAll.RestCotrollers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,42 +10,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.GameForAll.Repository.CategoryRepository;
+import com.GameForAll.Repository.CourseRepository;
+import com.GameForAll.Repository.TeacherRepository;
 import com.GameForAll.models.Category;
 import com.GameForAll.models.Course;
 import com.GameForAll.models.Teacher;
-import com.GameForAll.services.CategoryServices;
-import com.GameForAll.services.CourseServices;
-import com.GameForAll.services.TeacherServices;;
 
 @RestController
 public class CourseRestController {
 
 	@Autowired
-	private CourseServices courseServices;
-	
-	@Autowired
-	private CategoryServices categoryServices;
-	
-	@Autowired
-	private TeacherServices teacherServices;
+	private CourseRepository courseRepository;
 
-	@RequestMapping(value = "/showcourse/{CategoryId}", method = RequestMethod.GET)
-	public List<Course> ShowCourse(@PathVariable int CategoryId) {
-		return courseServices.GetAllCourses(CategoryId);
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private TeacherRepository teacherRepository;
+
+	@RequestMapping(value = "/course/show-courses/{CategoryId}", method = RequestMethod.GET)
+	public List<Course> ShowCourse(@PathVariable long CategoryId) {
+		Category category = categoryRepository.findOne(CategoryId);
+		List<Course> categories = new ArrayList<>();
+		if (category != null) {
+			categories = courseRepository.findByCategory(category);
+		}
+		return categories;
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "/addcourse/{categoryId}/{teacherId}")
-	public void AddCourse(@RequestBody Course c, @PathVariable int categoryId, @PathVariable int teacherId) {
-		/* Verification step */
-		c.setCategory(new Category( ""));
-		c.setTeacher(new Teacher( "", "", 0, "", "", ""));
-		if (categoryServices.FindCatgeory(categoryId) != null && teacherServices.FindTeacher(teacherId) != null) {
-			courseServices.addCourse(c);
+	@RequestMapping(method = RequestMethod.POST, value = "/course/addcourse/{categoryId}/{teacherId}")
+	public boolean AddCourse(@RequestBody Course course, @PathVariable long categoryId, @PathVariable long teacherId) 
+	{
+		Category category = categoryRepository.findOne(categoryId);
+		Teacher teacher = teacherRepository.findOne(teacherId);
+
+		if (category != null && teacher != null) {
+			course.setTeacher(teacher);
+			course.setCategory(category);
+			teacher.getCourses().add(course);
+			category.getCourses().add(course);
+			courseRepository.save(course);
+			return true;
 		} else {
-			System.out.println("Not Valid CategoryId or TeacherId!!");
+			return false;
 		}
 	}
-	
-	
 
 }
