aff632185fa74cb6bc8460acbae279a4024c85e6
Ehab Shebl <Ehab Shebl>
2017-04-15 17:26:06 +0200

    ehab-template-V4

--- SoftWare_2/phase2/GameForAll/src/main/resources/static/js/TypeController.js
+++ SoftWare_2/phase2/GameForAll/src/main/resources/static/js/TypeController.js
@@ -4,9 +4,10 @@
 app.controller('TypeTemplateController', function($scope, $http, $location) {
 	 $scope.CurrentLevel=1;
 	 $scope.CurrentQuestion=1;
-	 $scope.NumberOfAllQuestions=1;
+	 $scope.NumberOfAllQuestions=0;
 	 $scope.NameOfButtonLevel="Next level";
-	
+	 var id;	
+	 
 	$scope.UpdateQuestion = function(){
 		$scope.CurrentQuestion=$scope.CurrentQuestion+1;
 		$scope.NumberOfAllQuestions=$scope.NumberOfAllQuestions+1;
@@ -14,6 +15,8 @@ app.controller('TypeTemplateController', function($scope, $http, $location) {
 			$scope.NameOfButtonLevel="Finish Game";
 		}
     }
+	
+	
 	
 	$scope.UpdateLevel = function(){
 		if($scope.CurrentLevel==$scope.GameResponse.numOfLevels){
@@ -26,7 +29,7 @@ app.controller('TypeTemplateController', function($scope, $http, $location) {
 	
 	
 	$scope.getgame = function(){
-		var id=localStorage.getItem('id');
+		 id=localStorage.getItem('id');
 		var url = $location.absUrl()+"/get-game/"+id;
 		
         var config = {
@@ -44,14 +47,28 @@ app.controller('TypeTemplateController', function($scope, $http, $location) {
 
 	
     
-    $scope.AddGame = function(){
-        
-        
-        
-        
-       
-    }
-
+	 $scope.AddQuestion = function(){
+	        var url ="/create-question/"+id;
+	        var config = {
+	                headers : {
+	                    'Content-Type': 'application/json;charset=utf-8;'
+	                }
+	        }
+	        var data = {
+	        	question: $scope.Question,
+	        	gameId: id,
+	        	level: $scope.CurrentLevel
+	        };
+	        
+	        $http.post(url, data, config).then(function (AddResponse) {
+	        	$scope.AddResponse = AddResponse.data
+	        	
+	        	$scope.Question="";
+	        }, function (AddResponse) {
+	            $scope.postResultMessage = "Fail!";
+	        });
+	        
+	    }
 
 });
 
